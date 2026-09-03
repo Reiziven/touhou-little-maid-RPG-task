d0df4bbf8e03739e831dba8204b5c421e6643fd2
Reiziven <98499954+Reiziven@users.noreply.github.com>
2026-09-03 03:27:03 -0300

    Delete src/main directory

--- src/main/java/studio/fantasyit/maid_rpg_task/event/MaidTickEvent.java
+++
@@ -1,8 +0,0 @@
-package studio.fantasyit.maid_rpg_task.event;
-
-import net.minecraftforge.fml.common.Mod;
-import studio.fantasyit.maid_rpg_task.MaidRpgTask;
-
-@Mod.EventBusSubscriber(modid = MaidRpgTask.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-public class MaidTickEvent {
-}
