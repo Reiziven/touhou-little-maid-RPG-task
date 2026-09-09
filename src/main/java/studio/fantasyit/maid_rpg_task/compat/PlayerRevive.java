a1d5a7e51a10aca1db58f8f4b67b57dcbf8b454b
Reiziven <98499954+Reiziven@users.noreply.github.com>
2026-09-09 19:52:35 -0300

    Delete src/main/java directory

--- src/main/java/studio/fantasyit/maid_rpg_task/compat/PlayerRevive.java
+++
@@ -1,9 +0,0 @@
-package studio.fantasyit.maid_rpg_task.compat;
-
-import net.minecraftforge.fml.ModList;
-
-public class PlayerRevive {
-    public static boolean isEnable(){
-        return ModList.get().isLoaded("playerrevive");
-    }
-}
--- src/main/java/studio/fantasyit/maid_rpg_task/util/TranslateUtil.java
+++
@@ -1,9 +0,0 @@
-package studio.fantasyit.maid_rpg_task.util;
-
-import net.minecraft.network.chat.Component;
-
-public class TranslateUtil {
-    public static Component getBooleanTranslate(boolean b) {
-        return (b ? Component.translatable("gui.maid_useful_task.yes") : Component.translatable("gui.maid_useful_task.no"));
-    }
-}
